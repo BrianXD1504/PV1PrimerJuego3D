7de69f7ec01cacf629cf3d4ffd19e3105e672be4
Brian Estrada <165035871+BrianXD1504@users.noreply.github.com>
2025-09-27 20:49:31 -0300

    movimiento automatico del jugador cada 2 segundos

---
+++ Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovimiento : MonoBehaviour
+{
+    private Vector3 fuerzaPorAplicar;
+    private float tiempoDesdeUltimaFuerza;
+    private float intervaloTiempo;
+
+    void Start()
+    {
+        fuerzaPorAplicar = new Vector3(0, 0, 10);
+        tiempoDesdeUltimaFuerza = 0f;
+        intervaloTiempo = 2f;
+    }
+
+    void FixedUpdate()
+    {
+        tiempoDesdeUltimaFuerza += Time.fixedDeltaTime;
+
+        if (tiempoDesdeUltimaFuerza >= intervaloTiempo)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(fuerzaPorAplicar, ForceMode.Impulse);
+            tiempoDesdeUltimaFuerza = 0f;
+        }
+    }
+}
